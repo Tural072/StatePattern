f752adca302bcac1f6106945f65ee0b52b3ff66a
Tahi_ri96@itstep.edu.az <Tahi_ri96@itstep.edu.az>
2021-08-06 13:46:47 +0400

    done

---
+++ StatePattern/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+    public class Player
+    {
+        private State _state = null;
+        public Player(State state)
+        {
+
+        }
+    }
+}
---
+++ StatePattern/State.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+    abstract public class State
+    {
+        protected Player _player;
+        public void SetPlayer(Player player)
+        {
+            this._player = player;
+        }
+
+        public abstract void Handle1();
+
+        public abstract void Handle2();
+    }
+}
